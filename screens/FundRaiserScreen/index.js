079440b5538989d0c5d0a8e76ab2c23a0cb31f3f
Parth Chandna <pchandna24@gmail.com>
2020-01-31 23:10:32 +0530

    SJ

--- screens/FundRaiserScreen/index.js
+++ screens/FundRaiserScreen/index.js
@@ -6,7 +6,8 @@ import {
   ScrollView,
   Dimensions,
   Image,
-  TouchableOpacity
+  TouchableOpacity,
+  ActivityIndicator
 } from "react-native";
 import axios from "axios";
 import ProgressCircle from "react-native-progress-circle";
@@ -20,9 +21,13 @@ export default class FundRaiserScreen extends Component {
     amount: [],
     current: [],
     doctor: [],
-    cancerStage: []
+    cancerStage: [],
+    loading: true
   };
   async componentDidMount() {
+    this.setState({
+      loading: true
+    });
     console.log("hello");
     let name = [];
     let amount = [];
@@ -45,7 +50,8 @@ export default class FundRaiserScreen extends Component {
       amount: amount,
       current: current,
       doctor: doctor,
-      cancerStage: cancer
+      cancerStage: cancer,
+      loading: false
     });
     console.log(this.state.cancerStage);
     console.log(this.state.current);
@@ -54,271 +60,149 @@ export default class FundRaiserScreen extends Component {
     return (
       <View>
         <ScrollView>
-          {this.state.name.map((e, i) => {
-            return (
-              <View>
-                <View
-                  style={{
-                    backgroundColor: "#f9f9f9",
-                    borderRadius: 20,
-                    margin: 0.03 * HEIGHT,
-                    height: 0.4 * HEIGHT,
-                    width: 0.9 * WIDTH,
-                    shadowColor: "#000",
-                    shadowOffset: {
-                      width: 0,
-                      height: 12
-                    },
-                    shadowOpacity: 0.58,
-
-                    shadowRadius: 16.0,
-                    elevation: 24
-                  }}
-                >
-                  <Image
-                    style={{
-                      width: "28%",
-                      height: "28%",
-                      position: "absolute",
-                      borderRadius: 20,
-                      position: "absolute",
-                      resizeMode: "contain",
-                      left: -0.11 * WIDTH,
-                      top: -0.02 * HEIGHT,
-                      transform: [{ rotate: "-40deg" }]
-                    }}
-                    source={cancer}
-                  ></Image>
-                  <Text
-                    style={{
-                      marginTop: 0.09 * HEIGHT,
-                      marginLeft: 0.04 * WIDTH
-                    }}
-                  >
-                    <Text
-                      style={{
-                        fontSize: 22,
-                        fontWeight: "bold"
-                      }}
-                    >
-                      Name:
-                    </Text>
-                    <Text style={{ fontSize: 20 }}> {this.state.name[i]}</Text>
-                  </Text>
-                  <Text style={{ marginLeft: 0.04 * WIDTH }}>
-                    <Text
-                      style={{
-                        fontSize: 22,
-                        fontWeight: "bold"
-                      }}
-                    >
-                      Amount:
-                    </Text>
-                    <Text style={{ fontSize: 20 }}>
-                      {" "}
-                      {this.state.amount[i]}
-                    </Text>
-                  </Text>
-                  <Text style={{ marginLeft: 0.04 * WIDTH }}>
-                    <Text
-                      style={{
-                        fontSize: 22,
-                        fontWeight: "bold"
-                      }}
-                    >
-                      Doctor:
-                    </Text>
-                    <Text style={{ fontSize: 20 }}>
-                      {" "}
-                      {this.state.doctor[i]}
-                    </Text>
-                  </Text>
-                  <Text style={{ marginLeft: 0.04 * WIDTH }}>
-                    <Text
-                      style={{
-                        fontSize: 22,
-                        fontWeight: "bold"
-                      }}
-                    >
-                      Cancer Level:
-                    </Text>
-                    <Text style={{ fontSize: 20 }}>
-                      {" "}
-                      {this.state.cancerStage[i]}
-                    </Text>
-                  </Text>
+          {this.state.loading && <ActivityIndicator size="small" />}
+          {!this.state.loading &&
+            this.state.name.map((e, i) => {
+              return (
+                <View>
                   <View
                     style={{
-                      position: "absolute",
-                      right: 0.1 * WIDTH,
-                      top: 0.1 * HEIGHT
+                      backgroundColor: "#f9f9f9",
+                      borderRadius: 20,
+                      margin: 0.03 * HEIGHT,
+                      height: 0.4 * HEIGHT,
+                      width: 0.9 * WIDTH,
+                      shadowColor: "#000",
+                      shadowOffset: {
+                        width: 0,
+                        height: 12
+                      },
+                      shadowOpacity: 0.58,
+
+                      shadowRadius: 16.0,
+                      elevation: 24
                     }}
                   >
-                    <ProgressCircle
-                      percent={parseInt(this.state.current[i])}
-                      radius={42}
-                      borderWidth={6}
-                      color="#90ee90"
-                      shadowColor="#999"
-                      bgColor="#fff"
-                    >
-                      <Text style={{ fontSize: 18 }}>
-                        {this.state.current[i]}%
-                      </Text>
-                    </ProgressCircle>
-                  </View>
-                  <TouchableOpacity>
+                    <Image
+                      style={{
+                        width: "28%",
+                        height: "28%",
+                        position: "absolute",
+                        borderRadius: 20,
+                        position: "absolute",
+                        resizeMode: "contain",
+                        left: -0.11 * WIDTH,
+                        top: -0.02 * HEIGHT,
+                        transform: [{ rotate: "-40deg" }]
+                      }}
+                      source={cancer}
+                    ></Image>
                     <Text
                       style={{
-                        alignItems: "center",
-                        textAlign: "center",
-                        width: WIDTH - 220,
-                        fontWeight: "bold",
-                        fontSize: 15,
-                        height: 50,
-                        paddingTop: 15,
-                        color: "white",
-                        backgroundColor: "#404040",
-                        borderRadius: 10,
-                        marginLeft: WIDTH - 300,
-                        marginTop: 30,
-                        marginBottom: 15
+                        marginTop: 0.09 * HEIGHT,
+                        marginLeft: 0.04 * WIDTH
                       }}
                     >
-                      Donate
+                      <Text
+                        style={{
+                          fontSize: 22,
+                          fontWeight: "bold"
+                        }}
+                      >
+                        Name:
+                      </Text>
+                      <Text style={{ fontSize: 20 }}>
+                        {" "}
+                        {this.state.name[i]}
+                      </Text>
                     </Text>
-                  </TouchableOpacity>
+                    <Text style={{ marginLeft: 0.04 * WIDTH }}>
+                      <Text
+                        style={{
+                          fontSize: 22,
+                          fontWeight: "bold"
+                        }}
+                      >
+                        Amount:
+                      </Text>
+                      <Text style={{ fontSize: 20 }}>
+                        {" "}
+                        {this.state.amount[i]}
+                      </Text>
+                    </Text>
+                    <Text style={{ marginLeft: 0.04 * WIDTH }}>
+                      <Text
+                        style={{
+                          fontSize: 22,
+                          fontWeight: "bold"
+                        }}
+                      >
+                        Doctor:
+                      </Text>
+                      <Text style={{ fontSize: 20 }}>
+                        {" "}
+                        {this.state.doctor[i]}
+                      </Text>
+                    </Text>
+                    <Text style={{ marginLeft: 0.04 * WIDTH }}>
+                      <Text
+                        style={{
+                          fontSize: 22,
+                          fontWeight: "bold"
+                        }}
+                      >
+                        Cancer Level:
+                      </Text>
+                      <Text style={{ fontSize: 20 }}>
+                        {" "}
+                        {this.state.cancerStage[i]}
+                      </Text>
+                    </Text>
+                    <View
+                      style={{
+                        position: "absolute",
+                        right: 0.1 * WIDTH,
+                        top: 0.1 * HEIGHT
+                      }}
+                    >
+                      <ProgressCircle
+                        percent={parseInt(this.state.current[i])}
+                        radius={42}
+                        borderWidth={6}
+                        color="#90ee90"
+                        shadowColor="#999"
+                        bgColor="#fff"
+                      >
+                        <Text style={{ fontSize: 18 }}>
+                          {this.state.current[i]}%
+                        </Text>
+                      </ProgressCircle>
+                    </View>
+                    <TouchableOpacity>
+                      <Text
+                        style={{
+                          alignItems: "center",
+                          textAlign: "center",
+                          width: WIDTH - 220,
+                          fontWeight: "bold",
+                          fontSize: 15,
+                          height: 50,
+                          paddingTop: 15,
+                          color: "white",
+                          backgroundColor: "#404040",
+                          borderRadius: 10,
+                          marginLeft: WIDTH - 300,
+                          marginTop: 30,
+                          marginBottom: 15
+                        }}
+                      >
+                        Donate
+                      </Text>
+                    </TouchableOpacity>
+                  </View>
                 </View>
-              </View>
-            );
-          })}
-          <View>
-            <View
-              style={{
-                backgroundColor: "#f9f9f9",
-                borderRadius: 20,
-                margin: 0.03 * HEIGHT,
-                height: 0.4 * HEIGHT,
-                width: 0.9 * WIDTH,
-                shadowColor: "#000",
-                shadowOffset: {
-                  width: 0,
-                  height: 12
-                },
-                shadowOpacity: 0.58,
-
-                shadowRadius: 16.0,
-                elevation: 24
-              }}
-            >
-              <Image
-                style={{
-                  width: "28%",
-                  height: "28%",
-                  position: "absolute",
-                  borderRadius: 20,
-                  position: "absolute",
-                  resizeMode: "contain",
-                  left: -0.11 * WIDTH,
-                  top: -0.02 * HEIGHT,
-                  transform: [{ rotate: "-40deg" }]
-                }}
-                source={cancer}
-              ></Image>
-              <Text
-                style={{
-                  marginTop: 0.09 * HEIGHT,
-                  marginLeft: 0.04 * WIDTH
-                }}
-              >
-                <Text
-                  style={{
-                    fontSize: 22,
-                    fontWeight: "bold"
-                  }}
-                >
-                  Name:
-                </Text>
-                <Text style={{ fontSize: 20 }}> Ramesh</Text>
-              </Text>
-              <Text style={{ marginLeft: 0.04 * WIDTH }}>
-                <Text
-                  style={{
-                    fontSize: 22,
-                    fontWeight: "bold"
-                  }}
-                >
-                  Amount:
-                </Text>
-                <Text style={{ fontSize: 20 }}> 2500000</Text>
-              </Text>
-              <Text style={{ marginLeft: 0.04 * WIDTH }}>
-                <Text
-                  style={{
-                    fontSize: 22,
-                    fontWeight: "bold"
-                  }}
-                >
-                  Doctor:
-                </Text>
-                <Text style={{ fontSize: 20 }}> Dr. Dhama</Text>
-              </Text>
-              <Text style={{ marginLeft: 0.04 * WIDTH }}>
-                <Text
-                  style={{
-                    fontSize: 22,
-                    fontWeight: "bold"
-                  }}
-                >
-                  Cancer Level:
-                </Text>
-                <Text style={{ fontSize: 20 }}> 3</Text>
-              </Text>
-              <View
-                style={{
-                  position: "absolute",
-                  right: 0.1 * WIDTH,
-                  top: 0.1 * HEIGHT
-                }}
-              >
-                <ProgressCircle
-                  percent={37}
-                  radius={42}
-                  borderWidth={6}
-                  color="#90ee90"
-                  shadowColor="#999"
-                  bgColor="#fff"
-                >
-                  <Text style={{ fontSize: 18 }}>{"37%"}</Text>
-                </ProgressCircle>
-              </View>
-              <TouchableOpacity
-                onPress={() => {
-                  this.props.navigation.navigate("LiquidSwipe");
-                }}
-              >
-                <Text
-                  style={{
-                    alignItems: "center",
-                    textAlign: "center",
-                    width: WIDTH - 220,
-                    fontWeight: "bold",
-                    fontSize: 15,
-                    height: 50,
-                    paddingTop: 15,
-                    color: "white",
-                    backgroundColor: "#404040",
-                    borderRadius: 10,
-                    marginLeft: WIDTH - 300,
-                    marginTop: 30,
-                    marginBottom: 15
-                  }}
-                >
-                  Donate
-                </Text>
-              </TouchableOpacity>
-            </View>
-          </View>
+              );
+            })}
         </ScrollView>
       </View>
     );
